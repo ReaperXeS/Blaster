b28aba9524bcdaa966e4ddda12faf70bf9bb6082
Reaper XeS <reaperxes@gmail.com>
2022-04-29 21:10:39 -0400

    Overhead widget

--- Source/Blaster/GameMode/LobbyGameMode.cpp
+++ Source/Blaster/GameMode/LobbyGameMode.cpp
@@ -7,11 +7,16 @@ void ALobbyGameMode::PostLogin(APlayerController* aNewPlayer) {
 	Super::PostLogin(aNewPlayer);
 
 	int32 NumberOfPlayer = GameState.Get()->PlayerArray.Num();
+	GEngine->AddOnScreenDebugMessage(-1, 15.f, FColor::Yellow, FString::Printf(TEXT("Number of Players: %d"), GameState.Get()->PlayerArray.Num()));
 	if (NumberOfPlayer == 2) {
 		UWorld* World = GetWorld();
 		if (World) {
 			bUseSeamlessTravel = true;
-			World->ServerTravel(FString("/Game/Maps/BlasterMap?listen"));
+			FString MapAddress = FString("/Game/Maps/BlasterMap?listen");
+			GEngine->AddOnScreenDebugMessage(-1, 15.f, FColor::Yellow, FString::Printf(TEXT("Travelling to %s"), *MapAddress));
+			if (!World->ServerTravel(MapAddress)) {
+				GEngine->AddOnScreenDebugMessage(-1, 15.f, FColor::Red, FString::Printf(TEXT("Travelling to %s FAILED!!!"), *MapAddress));
+			}
 		}
 	}
 }
---
+++ Source/Blaster/HUB/OverheadWidget.cpp
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "OverheadWidget.h"
+#include "Components/TextBlock.h"
+#include "GameFramework/PlayerState.h"
+
+void UOverheadWidget::SetDisplayText(FString aTextToDisplay)
+{
+	if (DisplayText) {
+		DisplayText->SetText(FText::FromString(aTextToDisplay));
+	}
+}
+
+void UOverheadWidget::ShowPlayerNetRole(APawn* aPawn)
+{
+	ENetRole RemoteRole = aPawn->GetRemoteRole();
+	FString Role;
+	switch (RemoteRole)
+	{
+	case ENetRole::ROLE_Authority:
+		Role = FString("Authority");
+		break;
+	case ENetRole::ROLE_AutonomousProxy:
+		Role = FString("AutonomousProxy");
+		break;
+	case ENetRole::ROLE_SimulatedProxy:
+		Role = FString("SimulatedProxy");
+		break;
+	case ENetRole::ROLE_None:
+		Role = FString("None");
+		break;
+	}
+
+	FString RoleString = FString::Printf(TEXT("Remote Role: %s"), *Role);
+	SetDisplayText(RoleString);
+}
+
+void UOverheadWidget::ShowPlayerName(APawn* aPawn)
+{
+	SetDisplayText(aPawn->GetPlayerState()->GetPlayerName());
+}
+
+void UOverheadWidget::OnLevelRemovedFromWorld(ULevel* InLevel, UWorld* InWorld)
+{
+	RemoveFromParent();
+	Super::OnLevelRemovedFromWorld(InLevel, InWorld);
+}
