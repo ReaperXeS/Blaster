ae6593593fe2085f1f295980b63150209035b961
Reaper XeS <reaperxes@gmail.com>
2022-10-02 20:31:56 -0400

    Predict Projectile Path

--- Source/Blaster/Weapon/ProjectileBullet.cpp
+++ Source/Blaster/Weapon/ProjectileBullet.cpp
@@ -12,6 +12,8 @@ AProjectileBullet::AProjectileBullet()
 	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovementComponent"));
 	ProjectileMovementComponent->bRotationFollowsVelocity = true;
 	ProjectileMovementComponent->SetIsReplicated(true);
+	ProjectileMovementComponent->InitialSpeed = InitialSpeed;
+	ProjectileMovementComponent->MaxSpeed = InitialSpeed;
 }
 
 void AProjectileBullet::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
@@ -28,4 +30,26 @@ void AProjectileBullet::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, 
 
 	// Will be destroyed here
 	Super::OnHit(HitComp, OtherActor, OtherComp, NormalImpulse, Hit);
+}
+
+void AProjectileBullet::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FPredictProjectilePathParams PredictParams;
+	PredictParams.bTraceWithChannel = true;
+	PredictParams.bTraceWithCollision = true;
+	PredictParams.DrawDebugTime = 5.f;
+	PredictParams.DrawDebugType = EDrawDebugTrace::ForDuration;
+	PredictParams.LaunchVelocity = GetActorForwardVector() * InitialSpeed;
+	PredictParams.MaxSimTime = 4.f;
+	PredictParams.ProjectileRadius = 5.f;
+	PredictParams.SimFrequency = 30.f;
+	PredictParams.StartLocation = GetActorLocation();
+	PredictParams.TraceChannel = ECC_Visibility;
+	PredictParams.ActorsToIgnore.Add(this);
+
+	FPredictProjectilePathResult PredictResult;
+
+	UGameplayStatics::PredictProjectilePath(this, PredictParams, PredictResult);
 }
